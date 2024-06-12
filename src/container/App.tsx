907b17467beb0cd3dd7858f11fbe2225da1dc0ef
Mark Judson <mark.judson@leasequery.com>
2024-06-12 09:43:23 -0500

    clean up one

---
+++ src/container/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Footer, Header} from '../components/layout'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      Main Component
+      <Footer/>
+    </div>
+  )
+}
+
+export default App;
